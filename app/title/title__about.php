4c89d507142e4233dac0eae44f3c5ffd9d6da33b
Artem Tsys <19successful91@gmail.com>
2019-10-29 13:02:45 +0200

    documents + responsive

---
+++ app/title/title__about.php
@@ -0,0 +1,41 @@
+// ru version
+
+ <div class="title__name">
+        <div class="title__name__os-1">
+            <h3>
+                <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s" >П</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">Р</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">О</span></span>
+                <span class="text-left"><span class="my-wow title__two" data-animate="slideInUp" data-duration="0.5s" data-delay="0.3s">Компанию</span></span>
+            </h3>
+        </div>
+    </div>
+
+
+// ua version
+
+ <div class="title__name">
+        <div class="title__name__os-1">
+            <h3>
+                <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s" >П</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">Р</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">О</span></span>
+                <span class="text-left"><span class="my-wow title__two" data-animate="slideInUp" data-duration="0.5s" data-delay="0.3s">Компанію</span></span>
+            </h3>
+        </div>
+    </div>
+
+// en version
+
+     <div class="title__name">
+            <div class="title__name__os-1">
+                <h3>
+                    <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s" >A</span></span>
+                    <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">B</span></span>
+                    <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">О</span></span>
+                    <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">U</span></span>
+                    <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.7s">T</span></span>
+                    <span class="text-left"><span class="my-wow title__two" data-animate="slideInUp" data-duration="0.5s" data-delay="0.3s">Company</span></span>
+                </h3>
+            </div>
+        </div>
