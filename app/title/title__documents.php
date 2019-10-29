95228e5d740932fe7c1ee59b9244286c31c216fd
Artem Tsys <19successful91@gmail.com>
2019-10-29 14:09:22 +0200

    documents

---
+++ app/title/title__documents.php
@@ -0,0 +1,86 @@
+// ru version
+
+ <div class="title__name">
+        <div class="title__name__os-1">
+            <h3>
+                <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.55s" data-offset="0">Д</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">О</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">К</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">У</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.3s">М</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.5s">Е</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.7s">Н</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">Т</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">Ы</span></span>
+            </h3>
+        </div>
+    </div>
+
+
+// ua version
+
+
+ <div class="title__name">
+        <div class="title__name__os-1">
+            <h3>
+                <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.55s" data-offset="0">Д</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">О</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">К</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">У</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.3s">М</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.5s">Е</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.7s">Н</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">Т</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">И</span></span>
+            </h3>
+        </div>
+    </div>
+
+
+//en version
+
+ <div class="title__name">
+        <div class="title__name__os-1">
+            <h3>
+                <span class="text-left text-stroke"><span class="my-wow" data-animate="slideInRight" data-duration="0.55s" data-offset="0">D</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">O</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">C</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0">U</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.3s">M</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.5s">E</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="50" data-delay="0.7s">N</span></span>
+                <span class="title__block"></span>
+            </h3>
+        </div>
+        <div class="title__name__os-2">
+            <h3>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">T</span></span>
+                <span class="text-left"><span class="my-wow" data-animate="slideInRight" data-duration="0.4s" data-offset="0" data-delay="1s">S</span></span>
+            </h3>
+        </div>
+    </div>
